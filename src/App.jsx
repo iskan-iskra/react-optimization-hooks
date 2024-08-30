0f9daf1c6669643ec66b529c04f4e7d9f6812434
iskan-iskra <bf2iskan@gmail.com>
2024-08-30 21:45:53 +0500

    remove comments code

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,21 @@ import UseTransitionExample from "./components/useTransition";
 function App() {
   return (
     <>
-      {/* <h2>useCallback example</h2> */}
-      {/* <UseCallBackExample label={"world"} /> */}
-      {/* <hr /> */}
+      <h2>useCallback example</h2>
+      <UseCallBackExample label={"world"} />
+      <hr />
       <h2>UseSyncExternalStore example</h2>
       <UseSyncExternalStoreExample />
-
-      {/* <h2>memo example</h2> */}
-      {/* <MemoExample /> */}
-      {/* <hr /> */}
-      {/* <h2>useRef example</h2> */}
-      {/* <UseRefExample /> */}
-      {/* <hr /> */}
-      {/* <h2>useMemo example</h2> */}
-      {/* <UseMemoExample /> */}
-
-      {/* <h2>useTransition example</h2> */}
-
-      {/* <UseTransitionExample /> */}
+      <h2>memo example</h2>
+      <MemoExample />
+      <hr />
+      <h2>useRef example</h2>
+      <UseRefExample />
+      <hr />
+      <h2>useMemo example</h2>
+      <UseMemoExample />
+      <h2>useTransition example</h2>
+      <UseTransitionExample />
     </>
   );
 }
